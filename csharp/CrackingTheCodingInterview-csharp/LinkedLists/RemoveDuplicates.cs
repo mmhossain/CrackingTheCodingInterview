06d7ed089da39242df16e980dd13a0e5d0ba9240
mmhossain <mosharaf.hossain221@gmail.com>
2024-11-20 21:08:58 -0800

    Add solution for 2.1

---
+++ csharp/CrackingTheCodingInterview-csharp/LinkedLists/RemoveDuplicates.cs
@@ -0,0 +1,57 @@
+﻿namespace CrackingTheCodingInterview_csharp.LinkedLists;
+
+/*
+    2.1 Remove Dups: Write code to remove duplicates from an unsorted linked list.
+        FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed? 
+*/
+public class RemoveDuplicates
+{
+    /*
+        Time: O(n)
+        Space: O(n)
+            n = number of nodes in the Linked List
+    */
+    public void DeleteDups_ApproachI(SListNode node)
+    {
+        ISet<int> seen = new HashSet<int>();
+        SListNode previous = null;
+
+        while (node != null)
+        {
+            if (seen.Contains(node.Value))
+                previous.Next = node.Next;
+            else
+            {
+                seen.Add(node.Value);
+                previous = node;
+            }
+
+            node = node.Next;
+        }
+    }
+
+    /*
+        Time: O(n^2)
+        Space: O(1)
+            n = number of nodes in the Linked List
+    */
+    public void DeleteDups_ApproachII(SListNode node)
+    {
+        SListNode current = node;
+
+        while (current != null)
+        {
+            SListNode runner = current;
+
+            while (runner.Next != null)
+            {
+                if (runner.Next.Value == current.Value)
+                    runner.Next = runner.Next.Next;
+                else
+                    runner = runner.Next;
+            }
+
+            current = current.Next;
+        }
+    }
+}
--- csharp/CrackingTheCodingInterview-csharpTests/LinkedListsTests.cs
+++ csharp/CrackingTheCodingInterview-csharpTests/LinkedListsTests.cs
@@ -6,10 +6,12 @@ namespace CrackingTheCodingInterview_csharpTests;
 public class LinkedListsTests
 {
     private readonly DeleteSListNode _deleteSListNode;
+    private readonly RemoveDuplicates _removeDuplicates;
 
     public LinkedListsTests()
     {
-        _deleteSListNode = new DeleteSListNode();    
+        _deleteSListNode = new DeleteSListNode();
+        _removeDuplicates = new RemoveDuplicates();
     }
 
     [TestMethod]
@@ -24,5 +26,69 @@ public class LinkedListsTests
         // Assert
         Assert.AreEqual(1, updatedHead.Value);
         Assert.AreEqual(3, updatedHead.Next.Value);
+    }
+
+    [TestMethod]
+    public void DeleteDuplicates_ApproachITest()
+    {
+        // Arrange
+        SListNode head = new SListNode(1) { 
+            Next = new SListNode(2) { 
+                Next = new SListNode(2) { 
+                    Next = new SListNode(3) { 
+                        Next = new SListNode(4) { 
+                            Next = new SListNode(4) { 
+                                Next = new SListNode(5) 
+                            } 
+                        } 
+                    } 
+                } 
+            } 
+        };
+
+        // Act
+        _removeDuplicates.DeleteDups_ApproachI(head);
+
+        // Assert
+        Assert.AreEqual(1, head.Value);
+        Assert.AreEqual(2, head.Next.Value);
+        Assert.AreEqual(3, head.Next.Next.Value);
+        Assert.AreEqual(4, head.Next.Next.Next.Value);
+        Assert.AreEqual(5, head.Next.Next.Next.Next.Value);
+    }
+
+    [TestMethod]
+    public void DeleteDuplicates_ApproachIITest()
+    {
+        // Arrange
+        SListNode head = new SListNode(1)
+        {
+            Next = new SListNode(2)
+            {
+                Next = new SListNode(2)
+                {
+                    Next = new SListNode(3)
+                    {
+                        Next = new SListNode(4)
+                        {
+                            Next = new SListNode(4)
+                            {
+                                Next = new SListNode(5)
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // Act
+        _removeDuplicates.DeleteDups_ApproachII(head);
+
+        // Assert
+        Assert.AreEqual(1, head.Value);
+        Assert.AreEqual(2, head.Next.Value);
+        Assert.AreEqual(3, head.Next.Next.Value);
+        Assert.AreEqual(4, head.Next.Next.Next.Value);
+        Assert.AreEqual(5, head.Next.Next.Next.Next.Value);
     }
 }
