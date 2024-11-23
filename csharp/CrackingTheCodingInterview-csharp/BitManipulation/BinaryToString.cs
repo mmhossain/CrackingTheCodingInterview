cea221064667decc2f143ef93b3988dec4460a73
mmhossain <mosharaf.hossain221@gmail.com>
2024-11-23 08:36:20 -0800

    Add solution for 5.2

---
+++ csharp/CrackingTheCodingInterview-csharp/BitManipulation/BinaryToString.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace CrackingTheCodingInterview_csharp.BitManipulation;
+
+/*
+    5.2 Binary to String: Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a 
+        double, print the binary representation. If the number cannot be represented accurately in 
+        binary with at most 32 characters, print"ERROR:'
+*/
+public class BinaryToString
+{
+    /*
+        Time: O(1)
+        Space: O(1)
+    */
+    public string PrintBinary_ApproachI(double num)
+    {
+        if (num >= 1 || num <= 0)
+            return "ERROR";
+
+        StringBuilder binary = new StringBuilder();
+        binary.Append(".");
+
+        while (num > 0)
+        {
+            if (binary.Length >= 32)
+                return "ERROR";
+
+            double r = num * 2;
+            if (r >= 1)
+            {
+                binary.Append("1");
+                num = r - 1;
+            }
+            else
+            {
+                binary.Append("0");
+                num = r;
+            }
+        }
+
+        return binary.ToString();
+    }
+
+    /*
+        Time: O(1)
+        Space: O(1)
+    */
+    public string PrintBinary_ApproachII(double num)
+    {
+        if (num >= 1 || num <= 0)
+            return "ERROR";
+
+        StringBuilder binary = new StringBuilder();
+        binary.Append(".");
+
+        double fraction = 0.5;
+
+        while (num > 0)
+        {
+            if (binary.Length >= 32)
+                return "ERROR";
+
+            if (num >= fraction)
+            {
+                binary.Append("1");
+                num -= fraction;
+            }
+            else
+            {
+                binary.Append("0");
+            }
+
+            fraction /= 2;
+        }
+
+        return binary.ToString();
+    }
+}
--- csharp/CrackingTheCodingInterview-csharpTests/BitManipulationTests.cs
+++ csharp/CrackingTheCodingInterview-csharpTests/BitManipulationTests.cs
@@ -6,10 +6,12 @@ namespace CrackingTheCodingInterview_csharpTests;
 public class BitManipulationTests
 {
     private readonly Insertion _insertion;
+    private readonly BinaryToString _binaryToString;
 
     public BitManipulationTests()
     {
         _insertion = new Insertion();
+        _binaryToString = new BinaryToString();
     }
 
     [TestMethod]
@@ -23,5 +25,31 @@ public class BitManipulationTests
 
         // Assert
         Assert.AreEqual(0b10001001100, result);
+    }
+
+    [TestMethod]
+    public void PrintBinary_ApproachITest()
+    {
+        // Arrange
+        double num = 0.25;
+
+        // Act
+        string result = _binaryToString.PrintBinary_ApproachI(num);
+
+        // Assert
+        Assert.AreEqual(".01", result);
+    }
+
+    [TestMethod]
+    public void PrintBinary_ApproachIITest()
+    {
+        // Arrange
+        double num = 0.25;
+
+        // Act
+        string result = _binaryToString.PrintBinary_ApproachII(num);
+
+        // Assert
+        Assert.AreEqual(".01", result);
     }
 }
